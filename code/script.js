27f3b5783822183108c9a0b8f1c9d1853495e302
Camilla Borgqvist <camilla.borgqvist@banqsoft.com>
2025-04-22 14:16:07 +0200

    Step 2 done and tested

--- code/script.js
+++ code/script.js
@@ -3,22 +3,35 @@
 // Step 1 - Welcome and introduction
 // Your code goes here
 alert(
-  `Welcome to Mills Pizza House. Let's go ahead with your order. Ready to Start? - Click 'OK' to begin.`
+  "Welcome to Mills Pizza House. Let's go ahead with your order. Click 'OK' to begin."
 )
 {
 const name = prompt("Please enter your name to start with your order");
 if(name != null){
-  alert(
-    `Hello ${name}`);
+  alert(`Hello ${name}`);
 }
 }
-
-
 // Step 2 - Food choice
 // Your code goes here
+{
+  let foodChoise = prompt("Okey, what would you like to order today?\n Enter the number of you choise.\n 1. Pizza\n 2. Pasta\n 3. Salad");
+  if(foodChoise === "1"){
+  alert("You have picked Pizza")
+}
+  else if(foodChoise === "2"){
+  alert("You have picked Pasta")
+}
+  else if(foodChoise === "3"){
+  alert("You have picked Salad")
+}
+else{
+  alert("You have made a choise that doesn't exist. Try agian!")
+}
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
+
 
 // Step 4 - Age
 // Your code goes here
